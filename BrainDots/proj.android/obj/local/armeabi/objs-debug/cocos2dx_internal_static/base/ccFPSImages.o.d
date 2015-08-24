4ffabea891042a9d180518b6ee5fbf3e09b3a93f
reddevil_138 <reddevil138@gmail.com>
2015-08-25 02:18:05 +0900

    removed obj and libs

--- BrainDots/proj.android/obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/ccFPSImages.o: \
- D:\Github\BrainDots\BrainDots\proj.android\../cocos2d/cocos/./base/ccFPSImages.c \
- D:\Github\BrainDots\BrainDots\proj.android\../cocos2d/cocos/./base/ccFPSImages.h
-
-D:\Github\BrainDots\BrainDots\proj.android\../cocos2d/cocos/./base/ccFPSImages.h:
